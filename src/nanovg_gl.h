9f432033851ae11b2d164de4bf9b9812a9fbf9ba
Jan Ole Vollmer <jan.vollmer@hpi.de>
2018-11-07 22:24:25 +0100

    Make compatible with glbinding

--- src/nanovg_gl.h
+++ src/nanovg_gl.h
@@ -422,14 +422,14 @@ static int glnvg__createShader(GLNVGshader* shader, const char* name, const char
 
 	glCompileShader(vert);
 	glGetShaderiv(vert, GL_COMPILE_STATUS, &status);
-	if (status != GL_TRUE) {
+	if ((GLboolean) status != GL_TRUE) {
 		glnvg__dumpShaderError(vert, name, "vert");
 		return 0;
 	}
 
 	glCompileShader(frag);
 	glGetShaderiv(frag, GL_COMPILE_STATUS, &status);
-	if (status != GL_TRUE) {
+	if ((GLboolean) status != GL_TRUE) {
 		glnvg__dumpShaderError(frag, name, "frag");
 		return 0;
 	}
@@ -442,7 +442,7 @@ static int glnvg__createShader(GLNVGshader* shader, const char* name, const char
 
 	glLinkProgram(prog);
 	glGetProgramiv(prog, GL_LINK_STATUS, &status);
-	if (status != GL_TRUE) {
+	if ((GLboolean) status != GL_TRUE) {
 		glnvg__dumpProgramError(prog, name);
 		return 0;
 	}
